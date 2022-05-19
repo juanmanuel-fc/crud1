80bdd43e0168837d6a0088dfffd623affb784001
juanmanuel-fc <juanmanuel.fc@gmail.com>
2022-05-19 17:37:29 -0300

    product detail finalizado OK

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -9,37 +9,47 @@ const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
 const controller = {
 	// Root - Show all products
 	index: (req, res) => {
-		// Do the magic
+		res.render("products", {products: products})
 	},
 
 	// Detail - Detail from one product
 	detail: (req, res) => {
-		// Do the magic
+		
+		const id = parseInt(req.params.id) ;
+		console.log(id);
+		
+		foundProduct = products.filter ( product => {
+
+			return product.id === id;
+		})
+
+		res.render("detail", {product: foundProduct});
+
 	},
 
-	// Create - Form to create
-	create: (req, res) => {
-		// Do the magic
-	},
+	// // Create - Form to create
+	// create: (req, res) => {
+	// 	// Do the magic
+	// },
 	
-	// Create -  Method to store
-	store: (req, res) => {
-		// Do the magic
-	},
+	// // Create -  Method to store
+	// store: (req, res) => {
+	// 	// Do the magic
+	// },
 
-	// Update - Form to edit
-	edit: (req, res) => {
-		// Do the magic
-	},
-	// Update - Method to update
-	update: (req, res) => {
-		// Do the magic
-	},
+	// // Update - Form to edit
+	// edit: (req, res) => {
+	// 	// Do the magic
+	// },
+	// // Update - Method to update
+	// update: (req, res) => {
+	// 	// Do the magic
+	// },
 
-	// Delete - Delete one product from DB
-	destroy : (req, res) => {
-		// Do the magic
-	}
+	// // Delete - Delete one product from DB
+	// destroy : (req, res) => {
+	// 	// Do the magic
+	// }
 };
 
 module.exports = controller;
